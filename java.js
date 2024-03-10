83c4222c598ddfa0329ee6df35918d1c137d6628
danieltqza <danieltqza@gmail.com>
2024-03-09 22:24:06 -0500

    Changes

---
+++ java.js
@@ -0,0 +1,37 @@
+let moonbtn = document.getElementById('moonbtn');
+let body = document.body;
+let background = false;
+let sunbtn = document.getElementById('sunbtn');
+let header = document.getElementById('header');
+let aprov = document.getElementById('aprov')
+let aprov2 = document.getElementById('aprov2')
+
+
+    moonbtn.addEventListener('click', function() {
+        if (background == false) {
+            body.style.backgroundColor = '#111629';
+            header.style.color = 'white';
+            aprov.style.color = 'white'
+            aprov2.style.color = 'white'
+            moonbtn.hidden = true
+
+
+        } else {
+            body.style.backgroundColor = '#F2F9FF';
+            header.style.color = '#223343'
+
+            moonbtn.hidden = true
+        }
+    });
+
+    sunbtn.addEventListener('click', function() {
+        if (background){
+            body.style.backgroundColor = '#111629'
+            header.style.color = 'white';
+        } else{
+            body.style.backgroundColor = '#F2F9FF'
+            header.style.color = '#223343'
+            aprov.style.color = 'black';
+            aprov2.style.color = 'black';
+        }
+    })
